8a9e9dfef50089808f9be1e1b4eadbfbaf9f02c6
trams4 <trams4@illinois.edu>
2025-10-04 14:35:31 -0500

    added rounded rectangle for title!

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -4,8 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App" backgroundColor="rgb(102,152,101)">
-      
+      <div id="head"> <p id="head-text">B[U]ILT X Urban Planning Initiative</p></div>
     </div>
+    
   );
 }
 
