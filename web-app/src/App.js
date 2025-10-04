1ff662f914db7d0d54570e764243581ba4cc5c01
Esther Valentin <esthervalentin@Esthers-MacBook-Air-5.local>
2025-10-04 15:09:06 -0500

    new box under the title w text

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -5,7 +5,15 @@ function App() {
   return (
     <div className="App" backgroundColor="rgb(102,152,101)">
       <div id="head"> <p id="head-text">B[U]ILT X Urban Planning Initiative</p></div>
+
+      <div id="box">
+        <p>This website is a tool for urban planners that leverages data to provide appropriatte initiatives in the city of Chicago</p>
+      </div>
+
+      
     </div>
+
+ 
     
   );
 }
