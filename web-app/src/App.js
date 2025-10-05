14561779ab0d87a768459a658fa73050b4ca7105
Esther Valentin <esthervalentin@Esthers-MacBook-Air-5.local>
2025-10-05 00:03:44 -0500

    changed appearence

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -4,22 +4,23 @@ import './App.css';
 import ChatRunner from "./components/components";
 function App() {
   return (
-    <div className="App" backgroundColor="rgb(102,152,101)">
+    <div className="App" backgroundColor="rgb(0,91,77)">
       <div id="head"> <p id="head-text">B[U]ILT X Urban Planning Initiative</p>
       <a href="https://built-illinois.org/#/Home">
         <img id="built" src={builtLogo} alt="Built Logo" />
       </a>
       </div>
+    
 
     <div className="content-wrapper"> 
     
    
-        <div id="box">
-         <p>This website is a tool for urban planners that leverages data to provide appropriate initiatives in the city of Chicago</p>
+        <div id="box1">
+         <p>This website is a tool for urban planners that leverages data to provide appropriate initiatives in the city of Chicago. Our considerations include, but are not limited to, population density, pollution, and available green spaces.</p>
         </div>
  
         <div id="map">
-         <img src="chi-map.png" alt="Chicago Map" width="250" height="319.5" />
+         <img src="chi-map.png" alt="Chicago Map" width="766" height="458" />
         </div>
 
     </div>
@@ -27,19 +28,14 @@ function App() {
   
 
       <div className= "greenytro">
-        <div id="box">
+        <div id="box2">
           <p> As you use our urban planning tool we also have our personal assitant Greeny at your disposal!  </p>
            <ChatRunner />
         </div>
-        
-       
-
+        <div className="greeny-logo-container"></div>
+         <img src="rgreeny.png" alt="greeny" width="300" height="300"></img>
+        </div>
       </div>
-
-
-     
-      
-    </div>
 
  
     
